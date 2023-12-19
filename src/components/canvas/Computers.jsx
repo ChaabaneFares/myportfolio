2a1bd83ca71d538029fa3a70a7e2a744efdd45c0
ChaabenFares <chaaben.fares94@gmail.com>
2023-12-19 23:57:55 +0100

    decompression

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -29,7 +29,7 @@ const Computers = ({ isMobile }) => {
   );
 };
 const Mobiles = () => {
-  const mobile = useGLTF("./developer/model-draco.gltf");
+  const mobile = useGLTF("./developer/scene.gltf");
 
   return (
     <mesh >
