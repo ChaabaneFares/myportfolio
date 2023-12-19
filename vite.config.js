142d47d8e86425dc3bfa404ad45cc6a9f0e73313
ChaabenFares <chaaben.fares94@gmail.com>
2023-12-19 03:58:03 +0100

    conflict resolved

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory (default is 'dist')
+    sourcemap: true, // Generate source maps
+    minify: 'terser', // Specify minifier (e.g., 'terser', 'esbuild')
+    terserOptions: {
+      compress: {
+        drop_console: true, // Remove console logs in production
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Split vendor code into separate chunk
+        },
+      },
+    },
+  },
+});
