d912c1de251f38574cf470563221e331781596b3
ChaabenFares <chaaben.fares94@gmail.com>
2024-01-05 15:08:10 +0100

    android test

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -101,23 +101,23 @@ const ComputersCanvas = () => {
       </Suspense>
       <Preload all /> 
       </Canvas>):( 
-    //   <Canvas frameloop="demand"
-    //     shadows
-    //     dpr={[1, 2]}
-    //     camera={{ position: [20, 3, 5], fov: 25 }}
-    //     gl={{ preserveDrawingBuffer: true }}> 
-    //     <Suspense fallback={<CanvasLoader />}>
-    //     <OrbitControls
-    //       enableZoom={false}
-    //       maxPolarAngle={Math.PI / 2 }
-    //       minPolarAngle={Math.PI / 2}
+      <Canvas frameloop="demand"
+        shadows
+        dpr={[1, 2]}
+        camera={{ position: [20, 3, 5], fov: 25 }}
+        gl={{ preserveDrawingBuffer: true }}> 
+        <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2 }
+          minPolarAngle={Math.PI / 2}
         
-    //     />
-    //  <Computers isMobile={true}/>
-    //   </Suspense>
-    //   <Preload all /> 
-    //   </Canvas>
-       <></>
+        />
+     <Computers isMobile={true}/>
+      </Suspense>
+      <Preload all /> 
+      </Canvas>
+       
       )):( <Canvas
         frameloop="demand"
         shadows
