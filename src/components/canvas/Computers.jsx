a24ed45561ca0aa615f9c00346360fd20343dd59
ChaabenFares <chaaben.fares94@gmail.com>
2024-01-20 16:43:56 +0100

    try

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -111,12 +111,7 @@ const ComputersCanvas = () => {
         camera={{ position: [20, 3, 5], fov: 25 }}
         gl={{ preserveDrawingBuffer: true }}> 
         <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2 }
-          minPolarAngle={Math.PI / 2}
-        
-        />
+       
      <Mobiles isMobile={true}/>
       </Suspense>
       <Preload all /> 
