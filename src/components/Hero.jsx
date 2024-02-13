30e6f9ce6eea55798d4d45036fd43a5b24cca7bf
ChaabenFares <chaaben.fares94@gmail.com>
2024-02-13 01:26:57 +0100

    hamdullah

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,9 +22,9 @@ const Hero = () => {
       </div>
 
       {/* Transparent overlay */}
-      <div className="absolute inset-0 z-[1] bg-transparent opacity-0"></div>
+      <div className="absolute inset-0 z-[1] bg-transparent">
 
-      <div className="absolute xs:bottom-10 bottom-2 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-2 w-full flex justify-center items-center ">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
@@ -41,6 +41,7 @@ const Hero = () => {
             
           </div>
         </a>
+      </div>
       </div>
     </section>
   );
