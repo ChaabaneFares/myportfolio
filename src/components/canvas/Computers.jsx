0095d89ce45c2a59cc6789101c33a6c2c86b7839
ChaabenFares <chaaben.fares94@gmail.com>
2024-02-18 22:05:23 +0100

    changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -89,6 +89,7 @@ const ComputersCanvas = () => {
   return (
     <>
       {isMobile ? ( renderIOS ? (
+        <div className="absolute inset-0 z-[1] bg-transparent"> 
       <Canvas frameloop="demand"
         shadows
         dpr={[1, 2]}
@@ -103,7 +104,7 @@ const ComputersCanvas = () => {
         <Mobiles isMobile={true}/>
       </Suspense>
       <Preload all /> 
-      </Canvas>):( 
+      </Canvas></div> ):( 
         <div className='w-full h-auto absolute inset-0 z-[0]'>
        <StarsCanvas/>
        </div>
