3658de27e5d23dd35ddfac8e80b95538f51cf232
ChaabaneFares <chaaben.fares94@gmail.com>
2024-08-31 01:57:54 +0100

    about changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -58,7 +58,7 @@ const Works = () => {
 
       <div className="w-full flex">
         <motion.p
-          variants={fadeIn("", "spring", 0.5, 1)}
+          variants={fadeIn("", "spring", 0.5, 0.75)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Following projects showcases my skills and experience through
